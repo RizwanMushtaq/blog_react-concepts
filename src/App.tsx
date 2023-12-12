a6cdd1a438215a239364c46495deda89d0188fc4
kpms-rizwan-mushtaq <141311644+kpms-rizwan-mushtaq@users.noreply.github.com>
2023-12-12 11:44:44 +0100

    project clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import TestApp from "./chapters/Thinking in React/TestApp.tsx";
 
 function App() {
   return (
     <>
-      <h1>Custom Hooks Testing</h1>
-      <TestApp></TestApp>
+      <h1>React Concepts</h1>
     </>
   );
 }
